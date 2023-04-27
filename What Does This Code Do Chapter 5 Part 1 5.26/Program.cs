17ca34d83e6fcd0724d753476d375578dacb0c21
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-04-27 13:41:41 +0500

    solution done

--- What Does This Code Do Chapter 5 Part 1 5.26/Program.cs
+++ What Does This Code Do Chapter 5 Part 1 5.26/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class C5Q26
+{
+    static void Main(string[] args)
+    {
+        int row = 10;
+        int column;
+
+        while (row >= 1)
+        {
+            column = 1;
+
+            while (column <= 10)
+            {
+                Console.Write(row % 2 == 1? "<" : ">");
+
+                column++;
+            }
+            // end of inner loop
+
+            --row;
+
+            Console.WriteLine();
+        }
+        // end of outer loop
+    }
+}
